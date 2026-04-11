711e1a812a22996c9c4472b68ae42b45e5006ffd
SilentSlayer425 <fortnite2008n@gmail.com>
2026-04-11 15:05:17 -0700

    fixing

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { goToSubdomain } from '@/utils/navigation';
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
